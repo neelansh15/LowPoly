c7af472ae38627917722d40eaf05fe59ab3a2d37
devs4shah <devs4shah@gmail.com>
2022-02-27 19:51:33 +0530

    ft: New Dao without timelock

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.4.4;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,10 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
+describe("DAO", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const DAO = await ethers.getContractFactory("DAO");
+    const dao = await DAO.deploy("0x00");
+    await dao.deployed();
   });
 });
