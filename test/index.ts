81c63a125a20561ac8036762ce888239d851d905
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-02-27 19:54:11 +0530

    fix: `AddressZero`

--- test/index.ts
+++ test/index.ts
@@ -4,7 +4,7 @@ import { ethers } from "hardhat";
 describe("DAO", function () {
   it("Should return the new greeting once it's changed", async function () {
     const DAO = await ethers.getContractFactory("DAO");
-    const dao = await DAO.deploy("0x00");
+    const dao = await DAO.deploy(ethers.constants.AddressZero);
     await dao.deployed();
   });
 });
