30e9d3442cc9bdfccedcebb9ed53176174f36d17
devs4shah <devs4shah@gmail.com>
2022-02-27 19:55:41 +0530

    ft: Deployable dao contract

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("DAO", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  it("Should deploy", async function () {
     const DAO = await ethers.getContractFactory("DAO");
     const dao = await DAO.deploy(ethers.constants.AddressZero);
     await dao.deployed();
