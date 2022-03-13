ff105d60d0c70fbc32ae67439a0a320a28a84f16
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2022-03-13 22:31:48 +0530

    ft: Add connect wallet to navbar

--- frontend/src/components/ConnectWalletBtn.vue
+++ frontend/src/components/ConnectWalletBtn.vue
@@ -1,3 +1,35 @@
 <template>
-    <button>Connect Wallet</button>
+  <button @click="connectWallet">Connect to Wallet</button>
 </template>
+
+<script setup lang="ts">
+import { ethers } from "ethers";
+//chain id
+// eventHandler: accountChange, networkChange, disconnect
+// network change pe emit global
+
+async function connectWallet() {
+  const provider = window.ethereum;
+
+  const web3Provider = new ethers.providers.Web3Provider(
+    window.ethereum,
+    "any",
+  );
+  await web3Provider.send("eth_requestAccounts", []);
+  const network = await web3Provider.getNetwork();
+  const chainId = network.chainId;
+  console.log(network, chainId);
+  console.log({ accounts: web3Provider.listAccounts() });
+  const signer = web3Provider.getSigner();
+  let address = await signer.getAddress();
+  console.log("Account:", address);
+  provider.on("accountsChanged", (account: string[]) => {
+    address = account[0];
+    console.log({ changeAccount: account });
+  });
+  let balance = web3Provider.getBalance(address);
+  console.log({ balance: balance });
+}
+</script>
+
+<style></style>
