e779f39c66cf220ed9eebf3f1356c3fea01ddb6e
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-03-13 20:32:47 +0530

    ft: `HeaderCard` and import alias `~` tilde symbol

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+import HeaderCard from "~/components/HeaderCard.vue";
+</script>
+
 <template>
-    <div class="h-full">
-        <div class="p-10 bg-primary-600 text-white h-full">
-            <h1 class="pt-10 font-bold text-7xl">LowPoly</h1>
-            <p class="mt-2 text-gray-100">Transitioning organizations towards decentralization</p>
-        </div>
-    </div>
+  <div class="h-full">
+    <HeaderCard>
+      <h1 class="font-bold text-7xl">LowPoly</h1>
+      <p class="mt-2 text-gray-100">
+        Transitioning organizations towards decentralization
+      </p>
+    </HeaderCard>
+  </div>
 </template>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 
+// @ts-ignore
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), WindiCSS()],
+  resolve:{
+    alias:{
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 });
