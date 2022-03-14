b494e3de6d13c14a017bb0af545d78787819611d
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-03-14 11:34:15 +0530

    ft: update test with chai `expect`

--- test/index.ts
+++ test/index.ts
@@ -4,24 +4,19 @@ import { ethers } from "hardhat";
 describe("DAO", function () {
   it("Should deploy", async function () {
     const Token = await ethers.getContractFactory("Token");
-    const token = await Token.deploy("neelansh", "fbgm");
-    await token.deployed();
-
-    const DAO = await ethers.getContractFactory("DAO");
-    const dao1 = await DAO.deploy(token.address);
-    const dao2 = await dao1.deployed();
-    await dao2.votingDelay();
-  });
-});
-
-describe("Token", function () {
-  it("Should deploy token", async function () {
-    const Token = await ethers.getContractFactory("Token");
 
     const name = "TestToken";
     const symbol = "TT";
 
     const token = await Token.deploy(name, symbol);
     await token.deployed();
+
+    expect(token.name()).to.equal(name);
+    expect(token.symbol()).to.equal(symbol);
+
+    const DAO = await ethers.getContractFactory("DAO");
+    const dao1 = await DAO.deploy(token.address);
+    const dao2 = await dao1.deployed();
+    await dao2.votingDelay();
   });
 });
