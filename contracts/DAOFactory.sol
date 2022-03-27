346f490141003978f7981a8b91f12c9793e3b29a
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-03-27 22:11:59 +0530

    ft: DAO name

--- contracts/DAOFactory.sol
+++ contracts/DAOFactory.sol
@@ -4,19 +4,22 @@ pragma solidity ^0.8.4;
 import "./DAO.sol";
 
 contract DAOFactory {
-  // Mapping of owner addresses to their created DAOs
-  mapping(address => address[]) public _daos;
+    // Mapping of owner addresses to their created DAOs
+    mapping(address => address[]) public _daos;
 
-  event NewDAO(address _daoAddress, address _tokenAddress);
+    event NewDAO(address _daoAddress, address _tokenAddress);
 
-  function createDAO(address tokenAddress) public returns (DAO) {
-    DAO dao = new DAO(tokenAddress);
-    _daos[msg.sender].push(address(dao));
-    emit NewDAO(address(dao), address(tokenAddress));
-    return dao;
-  }
+    function createDAO(string memory name, IVotes tokenAddress)
+        public
+        returns (DAO)
+    {
+        DAO dao = new DAO(name, tokenAddress);
+        _daos[msg.sender].push(address(dao));
+        emit NewDAO(address(dao), address(tokenAddress));
+        return dao;
+    }
 
-  function getDAOs() public view returns (address[] memory) {
-    return _daos[msg.sender];
-  }
+    function getDAOs() public view returns (address[] memory) {
+        return _daos[msg.sender];
+    }
 }
