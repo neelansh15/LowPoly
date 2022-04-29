245d3d40880413a243d6ff308b9fee3ec88acbd2
Vedant Mahadik <mahadikvedant32@gmail.com>
2022-04-29 20:11:34 +0530

    ft: add proposals p1

---
+++ frontend/src/components/DAOCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="m-5 p-10 bg-primary-600 text-white max-h-50 min-w-30 max-w-sm">
+    <h1 class="text-3xl text-center font-bold">
+      <slot name="name"></slot>
+    </h1>
+    <p class="text-xl text-center font-bold">
+      <slot name="token"></slot>
+    </p>
+  </div>
+</template>
