7cfdad7b3a8653914cbd8273a597d540cc29f605
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-05-01 18:31:10 +0530

    ft: dex test withdrawAll

--- test/dex.ts
+++ test/dex.ts
@@ -3,7 +3,7 @@ import { formatEther, parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 describe.only("LowPoly DEX", function () {
-  it("Should give tokens to DEX and buy some of them", async function () {
+  it("Should give tokens to DEX, buy and withdraw", async function () {
     const dexFactory = await ethers.getContractFactory("DEX");
     const dex = await dexFactory.deploy();
 
@@ -38,5 +38,16 @@ describe.only("LowPoly DEX", function () {
     // let userTokenBalance = +formatEther(await token.balanceOf(owner.address));
     // console.log("user token balance after all this", userTokenBalance);
     // expect(userTokenBalance).to.equal(+totalSupply - 1000 + amount);
+
+    // Withdraw
+    await dex.withdrawAll(token.address);
+    const thisTokenEtherBalanceAfterWithdraw = await dex.tokenEtherBalance(
+      token.address
+    );
+    console.log(
+      "DEX token ether balance after withdraw",
+      formatEther(thisTokenEtherBalanceAfterWithdraw)
+    );
+    expect(+formatEther(thisTokenEtherBalanceAfterWithdraw)).to.equal(0);
   });
 });
